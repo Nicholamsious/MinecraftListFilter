71d3479ef0568df71395f8602b304390a2b91be2
Nick Williams <accounts@nickinnovates.com>
2025-01-26 14:15:27 +0000

    Rename "Hay Block" to "Hay Bale"

--- src/Main.java
+++ src/Main.java
@@ -158,6 +158,8 @@ public class Main {
                         finalName = "Bottle o' Enchanting";
                     } else if(finalName.equals("Jack O Lantern")) {
                         finalName = "Jack o'Lantern";
+                    } else if(finalName.equals("Hay Block")) {
+                        finalName = "Hay Bale";
                     } else if(finalName.equals("Flower Banner Pattern")) {
                         finalName = "Flower Charge Banner Pattern";
                     } else if(finalName.equals("Creeper Banner Pattern")) {
